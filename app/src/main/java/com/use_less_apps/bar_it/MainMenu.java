45991745d33c2c859b589d792a99768b9dda10a6
datrueonejay <jayarquelada@gmail.com>
2017-05-06 19:49:32 -0400

    First commit, created blank main menu

---
+++ app/src/main/java/com/use_less_apps/bar_it/MainMenu.java
@@ -0,0 +1,13 @@
+package com.use_less_apps.bar_it;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+}
